af1bc6f0843e0ff211eb11b6eaa03db2b18ca896
Brianna McGovern <bmcgovern007@gmail.com>
2020-03-23 18:41:32 -0700

    Initial Commit

---
+++ Font Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Font Viewer
+//
+//  Created by Brianna McGovern on 3/23/20.
+//  Copyright © 2020 Brianna McGovern. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Font Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Font Viewer
+//
+//  Created by Brianna McGovern on 3/23/20.
+//  Copyright © 2020 Brianna McGovern. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
